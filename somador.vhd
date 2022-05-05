cc99f72a826f8adf823d5dd2f11b1b2bc2396493
Laura Maciel Neves Franco <62398166+lauramnf@users.noreply.github.com>
2022-05-05 18:29:41 -0300

    somador v1

---
+++ somador.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity somador is
+
+	port 
+	(
+		entrada_A: in std_logic_vector  (31 downto 0);
+		entrada_B: in std_logic_vector  (31 downto 0);
+		saida: out std_logic_vector(31 downto 0)
+	);
+
+end entity;
+
+architecture a of somador is
+begin
+
+	saida <= std_logic_vector(signed(entrada_A) + signed(entrada_B));
+
+end a;
